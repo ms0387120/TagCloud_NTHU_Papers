4a99883c8091591f21d9d1944c014536588dd5d8
Mei-Ju Su <ms0387120@yahoo.com.tw>
2017-03-12 22:44:19 +0800

    Add files via upload

---
+++ wordcount.py
@@ -0,0 +1,24 @@
+from collections import Counter
+file=open("C:/Users/user/Desktop/作業二/building_global_community.txt","r")
+wordCounter= {}
+for line in file:
+	word_list = line.replace(',',' ,').replace('?',' ?').replace('.',' .').replace(':',' : ').replace('"',' " ').replace(';',' ; ').replace('\'',' \'').lower().split()
+	for word in word_list:
+		if word not in wordCounter:
+			wordCounter[word]= 1
+		else:
+			wordCounter[word]+=1
+wordCounter= Counter(wordCounter)
+wordCounter.most_common(20)
+file.close()
+
+
+
+
+
+
+
+
+
+
+
